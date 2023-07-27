358a74bbb215bb39a57c24132afffdd75cdb908c
lasnikr <lasnikprogram@gmail.com>
2023-07-28 01:37:16 +0200

    IO test

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,24 @@
 module Main (main) where
 
+import System.Environment
+import Data.Char
 
 main :: IO ()
-main = putStrLn ("Hello, world!" :: String)
+main = do
+   args <- getArgs
+   case args of
+      (filePath:_)   -> getFile filePath
+      _              -> putStrLn "[Interactive Mode]"
+
+getFile :: String -> IO ()
+getFile filePath = do
+   contents <- readFile filePath
+   putStrLn $ parseLine (lines contents)
+
+parseLine :: [String] -> String
+parseLine = concatMap (parseToken . map toLower)
+
+parseToken :: String -> String
+parseToken token = case token of
+   "add" -> "adding\n"
+   _     -> "nothing\n"
