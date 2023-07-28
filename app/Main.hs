c11c004903595328649fb498e25e1a7640c92eea
Lasnik <lasnikprogram@gmail.com>
2023-07-28 22:58:33 +0200

    interactive mode

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,40 @@
 module Main (main) where
 
+import System.Console.Haskeline;
 import System.Environment
 import Data.Char
+import Control.Monad.IO.Class
 
 main :: IO ()
 main = do
    args <- getArgs
    case args of
       (filePath:_)   -> getFile filePath
-      _              -> putStrLn "[Interactive Mode]"
+      _              -> interactiveMode
+
+interactiveMode :: IO ()
+interactiveMode = 
+   runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "> "
+           case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just input -> do 
+                           liftIO $ putStrLn $ parseLine input
+                           loop
 
 getFile :: String -> IO ()
 getFile filePath = do
    contents <- readFile filePath
-   putStrLn $ parseLine (lines contents)
+   putStrLn $ parseLine (concat (lines contents))
 
-parseLine :: [String] -> String
-parseLine = concatMap (parseToken . map toLower)
+parseLine :: String -> String
+parseLine = parseToken . map toLower
 
-parseToken :: String -> String
+parseToken :: String -> String 
 parseToken token = case token of
    "add" -> "adding\n"
    _     -> "nothing\n"
