08d9fedf2134ed2a3aa0fa52294a1f284b504fee
lasnikr <lasnikprogram@gmail.com>
2023-08-19 16:35:01 +0200

    ByteString as output, basic error handling

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,8 @@ import Data.Char
 import System.Console.Haskeline
 import System.Environment
 import Numeric
+import qualified Data.ByteString as B
+import Data.List
 
 type MachineCode = String
 data LookUp = LookUp String String
@@ -27,74 +29,103 @@ countChar c (x:xs)
     | c == x = 1 + countChar c xs
     | otherwise = countChar c xs
 
+binaryChunkToByte :: MachineCode -> Int
+binaryChunkToByte st = foldl' (\x acc -> x * 2 + acc ) 0 (map digitToInt st)
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs : chunksOf n (drop n xs)
+
+littleEndian :: [MachineCode] -> [MachineCode]
+littleEndian [] = []
+littleEndian [x] = [x]
+littleEndian (x:y:xs) = y : x : littleEndian xs
+
+binaryStringToByteString :: MachineCode -> B.ByteString
+binaryStringToByteString binary =
+    B.pack $ map (fromIntegral . binaryChunkToByte) $ littleEndian $ chunksOf 8 binary
+
+insertUnderscores :: MachineCode -> MachineCode
+insertUnderscores [] = []
+insertUnderscores xs = let (chunk, rest) = splitAt 4 xs
+                        in
+                            if null rest then chunk
+                            else chunk ++ "_" ++ insertUnderscores rest
+
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        (filePath:_) -> getFile filePath
-        _ -> interactiveMode
+        (inputPath:outputPath:_) -> getFile inputPath outputPath
+        _ -> do
+            putStrLn "Entering interactive mode."
+            putStrLn "To use the assembler on a file, run 'nxasm [inputPath] [outputPath]'"
+            interactiveMode
 
 interactiveMode :: IO ()
 interactiveMode =
     runInputT defaultSettings loop
     where
-      loop :: InputT IO ()
-      loop = do
-          minput <- getInputLine "> "
-          case minput of
-              Nothing -> return ()
-              Just "quit" -> return ()
-              Just "exit" -> return ()
-              Just input -> do
-                  liftIO $ putStrLn $ parseLine input
-                  loop
+        loop :: InputT IO ()
+        loop = do
+            minput <- getInputLine "> "
+            case minput of
+                Nothing -> return ()
+                Just "quit" -> return ()
+                Just "exit" -> return ()
+                Just input -> do
+                    liftIO $ putStrLn $ insertUnderscores $ parseLine input
+                    loop
 
-getFile :: String -> IO ()
-getFile filePath = do
-    contents <- readFile filePath
-    putStrLn $ concatMap parseLine (lines contents)
+getFile :: FilePath -> FilePath  -> IO ()
+getFile inputPath outputPath = do
+    contents <- readFile inputPath
+    B.writeFile outputPath $ binaryStringToByteString $ concatMap parseLine (lines contents)
 
 parseLine :: String -> MachineCode
 parseLine line
     | null line = ""
     | otherwise =
         let wordsArr = words $ map toLower line
-        in 
-            case head wordsArr of
+            mnemonic = head wordsArr
+        in
+            case mnemonic of
                 "add" -> parseOperands line [LookUp "r%n,r%m" "1000nnnnmmmm0001", LookUp "r%n,%i" "0001nnnniiiiiiii"]
                 "or" -> parseOperands line [LookUp "r%n,r%m" "1000nnnnmmmm0011", LookUp "r%n,%i" "0011nnnniiiiiiii"]
-                _ -> ""
+                _ -> error $ "No such mnemonic: '" ++ mnemonic ++ "'"
 
 parseOperands :: String -> [LookUp] -> MachineCode
 parseOperands line lookUps =
-    let cleanLine = filter (/= ' ') . concat . tail $ words line
-        matchingLookUps = [(result, vars) | LookUp prefix result <- lookUps, let (matches, vars) = matchesPrefix prefix cleanLine, matches]
+    let withoutMnemonic = concat . tail $ words line
+        cleanLine = filter (/= ' ') withoutMnemonic
+        matchingLookUps = [(result, vars) | LookUp prefix result <- lookUps, let (matches, vars) = matchLookups prefix cleanLine, matches]
     in case matchingLookUps of
-        [] -> ""
+        [] -> error $ "Couldn't parse operands: '" ++ withoutMnemonic ++ "' for '" ++ head (words line) ++ "'"
         ((result, vars):_) -> processVariables result vars
 
 processVariables :: String -> [Variable] -> MachineCode
 processVariables result [] = result
 processVariables result (Variable char intValue : vars) =
     let binary = showBinary intValue
-        len = countChar char result
+        availableLength = countChar char result
+        inputLength = length binary
     in
-        case compare (length binary) len of
-            GT -> ""
+        case compare inputLength availableLength of
+            GT -> error $ "Number not in valid range: " ++ show inputLength ++ " > " ++ show availableLength
             EQ -> processVariables (replaceChars result char binary) vars
-            LT -> processVariables (replaceChars result char (replicate (len - length binary) '0' ++ binary)) vars
+            LT -> processVariables (replaceChars result char (replicate (availableLength - inputLength) '0' ++ binary)) vars
 
-matchesPrefix :: String -> String -> (Bool, [Variable])
-matchesPrefix [] [] = (True, [])
-matchesPrefix ('%':holder:restPrefix) restLine =
+matchLookups :: String -> String -> (Bool, [Variable])
+matchLookups [] [] = (True, [])
+matchLookups ('%':holder:restPrefix) restLine =
     case span isDigit restLine of
         ([], _) -> (False, [])
         (digits, remaining) ->
             let varValue = read digits
                 variable = Variable holder varValue
-                (success, variables) = matchesPrefix restPrefix remaining
+                (success, variables) = matchLookups restPrefix remaining
             in
                 (success, variable : variables)
-matchesPrefix (x:xs) (y:ys) =
-    if x == y then matchesPrefix xs ys else (False, [])
-matchesPrefix _ _ = (False, [])
+matchLookups (x:xs) (y:ys) =
+    if x == y then matchLookups xs ys else (False, [])
+matchLookups _ _ = (False, [])
